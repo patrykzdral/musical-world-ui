551ae8e1f703dfb652512a8d6720f415c1f245e8
patryk.zdral <patryk.zdral@capgemini.com>
2018-11-01 21:02:22 +0100

    code update

---
+++ src/app/@core/service/google/google.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import {} from '@types/googlemaps';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GoogleService {
+
+  constructor() { }
+
+  getGeoLocation(lat: number, lng: number) {
+    console.log(lat);
+    console.log(lng);
+    if (!lat || !lng) {
+      console.log("empty ");
+      return null;
+    }
+
+
+    if (navigator.geolocation) {
+      let geocoder = new google.maps.Geocoder;
+      let latlng = new google.maps.LatLng(lat, lng);
+
+      geocoder.geocode({'location': latlng}, (results, status) => {
+        if (status == google.maps.GeocoderStatus.OK) {
+          let result = results[0];
+          let rsltAdrComponent = result.address_components;
+          let resultLength = rsltAdrComponent.length;
+          if (result != null) {
+            return result;
+          } else {
+            alert("No address available!");
+            return null;
+          }
+        }
+      });
+    }
+  }
+}
